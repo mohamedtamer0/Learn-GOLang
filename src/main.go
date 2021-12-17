0f9a18a6e773bfd64d8b2d05fad2de5dcde8850d
mohamedtamer0 <mt4452242@gmail.com>
2021-12-17 03:38:51 +0200

    update

--- src/main.go
+++ src/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var i int
+	i = 25
+	i = 26
+
+	var x = 30
+	y := 20
 	fmt.Println("Hello World!")
+	fmt.Println(i)
+	fmt.Println(x)
+	fmt.Println(y)
 }
