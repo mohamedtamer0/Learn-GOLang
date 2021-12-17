4a40b668ebd7415a1bf3dbec80094067025dfdee
mohamedtamer0 <mt4452242@gmail.com>
2021-12-17 21:50:11 +0200

    update

--- src/Constants.go
+++ src/Constants.go
@@ -15,9 +15,36 @@ Enumeration constants
 const aa int16 = 27
 
 const (
-	one   = iota
-	two   = iota
-	three = iota
+	one = iota
+	two
+	three
+	//OR
+	//two   = iota
+	//three = iota
+)
+
+const (
+	data1 = iota
+	data2
+	data3
+)
+
+const (
+	t = iota + 5
+	tt
+	ttt
+)
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
 )
 
 func main() {
@@ -47,5 +74,19 @@ func main() {
 	fmt.Printf("%v \n", one)
 	fmt.Printf("%v \n", two)
 	fmt.Printf("%v \n", three)
+
+	fmt.Println("===============")
+	var dataTest int = data2
+	fmt.Printf("%v \n", dataTest == data2)
+
+	fmt.Println("===============")
+	fmt.Printf("%v \n", t)
+	fmt.Printf("%v \n", tt)
+	fmt.Printf("%v \n", ttt)
+
+	fmt.Println("===============")
+
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB", fileSize/GB)
 
 }
