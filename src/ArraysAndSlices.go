4774de20017342d9cc1bf033cc645f192c5904e5
mohamedtamer0 <mt4452242@gmail.com>
2021-12-19 02:52:39 +0200

    update

---
+++ src/ArraysAndSlices.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+Arrays:
+Creation
+built-in function
+working with arrays
+Slices:
+Creation
+built-in function
+Working with slices
+*/
+func main() {
+	grades := [3]int{97, 85, 93}
+	fmt.Printf("Grades: %v", grades)
+}
